46a1e786450fc0ce57ace6a6a5d911d6cad6f983
Lukas Simon <lukas@flecs.tech>
2026-01-26 13:25:09 +0100

    Introduce tokio single threaded runtime

--- entrypoint/src/main.rs
+++ entrypoint/src/main.rs
@@ -1,12 +1,12 @@
-use std::thread::sleep;
 use std::time::Duration;
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     println!("Hello");
     // TODO: Initialize signal handler
     // TODO: Start flecs-core, flecs-webapp and flecs-floxy
     // TODO: Wait for signal
     // TODO: Shutdown flecs-core, flecs-webapp and flecs-floxy
-    sleep(Duration::from_secs(1));
+    tokio::time::sleep(Duration::from_secs(1)).await;
     println!("Goodbye");
 }
