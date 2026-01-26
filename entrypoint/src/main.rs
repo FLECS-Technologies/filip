52e0dc9449fda606585836b4a1738ec98e7db065
Lukas Simon <lukas@flecs.tech>
2026-01-26 13:44:14 +0100

    Init signal handler for shutdown signals

--- entrypoint/src/main.rs
+++ entrypoint/src/main.rs
@@ -1,12 +1,34 @@
-use std::time::Duration;
+use async_signal::{Signal, Signals};
+use futures_util::StreamExt;
+
+fn init_signal_handler() -> std::io::Result<tokio::sync::oneshot::Receiver<()>> {
+    let (result_sender, result_receiver) = tokio::sync::oneshot::channel();
+    let mut signals = Signals::new([Signal::Term, Signal::Int, Signal::Quit])?;
+    tokio::spawn(async move {
+        println!("Signal handler was initialized");
+        while let Some(signal) = signals.next().await {
+            println!("Received signal {signal:?}");
+            if matches!(
+                signal,
+                Ok(Signal::Int) | Ok(Signal::Term) | Ok(Signal::Quit)
+            ) {
+                result_sender.send(()).unwrap();
+                break;
+            }
+        }
+    });
+    Ok(result_receiver)
+}
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     println!("Hello");
-    // TODO: Initialize signal handler
+    let stop_signal = init_signal_handler()?;
     // TODO: Start flecs-core, flecs-webapp and flecs-floxy
-    // TODO: Wait for signal
+    stop_signal
+        .await
+        .expect("The sending side is never dropped before sending");
     // TODO: Shutdown flecs-core, flecs-webapp and flecs-floxy
-    tokio::time::sleep(Duration::from_secs(1)).await;
     println!("Goodbye");
+    Ok(())
 }
