acf4a736024a082e6db25fd6e24d23135d297c4a
Lukas Simon <lukas@flecs.tech>
2026-01-28 09:23:08 +0100

    Introduce main Error enum using thiserror

--- entrypoint/src/main.rs
+++ entrypoint/src/main.rs
@@ -9,26 +9,33 @@ use crate::docker::volume::create_floxy_data_volume;
 use crate::signal::init_signal_handler;
 use docker::network::create_network;
 
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    Bollard(#[from] bollard::errors::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     info!("Hello");
     let stop_signal = init_signal_handler()?;
-    // TODO: Handle errors instead of unwrapping
-    let docker_client = docker_client().unwrap();
-    let gateway = create_network(&docker_client).await.unwrap();
-    create_floxy_data_volume(&docker_client).await.unwrap();
-    create_containers(&docker_client.clone(), gateway)
-        .await
-        .unwrap();
-    start_containers(&docker_client).await.unwrap();
+    let docker_client = docker_client()?;
+    let gateway = create_network(&docker_client).await?;
+    create_floxy_data_volume(&docker_client).await?;
+    create_containers(&docker_client.clone(), gateway).await?;
+    start_containers(&docker_client).await?;
     stop_signal
         .await
         .expect("The sending side is never dropped before sending");
     info!("Stopping containers...");
-    stop_containers(&docker_client).await.unwrap();
+    stop_containers(&docker_client).await?;
     info!("...stopped containers");
     info!("Removing containers...");
-    remove_containers(&docker_client).await.unwrap();
+    remove_containers(&docker_client).await?;
     info!("...removed containers");
     info!("Goodbye");
     Ok(())
