778ea7b38d7fea4d19a4656637ff5efa553e75fc
Lukas Simon <lukas@flecs.tech>
2026-01-28 11:39:11 +0100

    signal: Listen only for SIGTERM and SIGINT

--- entrypoint/src/signal.rs
+++ entrypoint/src/signal.rs
@@ -4,15 +4,12 @@ use futures_util::StreamExt;
 
 pub fn init_signal_handler() -> std::io::Result<tokio::sync::oneshot::Receiver<()>> {
     let (result_sender, result_receiver) = tokio::sync::oneshot::channel();
-    let mut signals = Signals::new([Signal::Term, Signal::Int, Signal::Quit])?;
+    let mut signals = Signals::new([Signal::Term, Signal::Int])?;
     tokio::spawn(async move {
         info!("Signal handler was initialized");
         while let Some(signal) = signals.next().await {
             info!("Received signal {signal:?}");
-            if matches!(
-                signal,
-                Ok(Signal::Int) | Ok(Signal::Term) | Ok(Signal::Quit)
-            ) {
+            if matches!(signal, Ok(Signal::Int) | Ok(Signal::Term)) {
                 result_sender.send(()).unwrap();
                 break;
             }
