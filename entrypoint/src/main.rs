516e6b47787005f5f3ee304c65e8eadd41b046b6
Lukas Simon <lukas@flecs.tech>
2026-01-28 09:16:14 +0100

    Move signal init to signal module

--- entrypoint/src/main.rs
+++ entrypoint/src/main.rs
@@ -1,33 +1,13 @@
-use async_signal::{Signal, Signals};
-use futures_util::StreamExt;
-
 mod docker;
 mod logging;
+mod signal;
 use crate::docker::container::{
     create_containers, remove_containers, start_containers, stop_containers,
 };
 use crate::docker::docker_client;
 use crate::docker::volume::create_floxy_data_volume;
+use crate::signal::init_signal_handler;
 use docker::network::create_network;
-
-fn init_signal_handler() -> std::io::Result<tokio::sync::oneshot::Receiver<()>> {
-    let (result_sender, result_receiver) = tokio::sync::oneshot::channel();
-    let mut signals = Signals::new([Signal::Term, Signal::Int, Signal::Quit])?;
-    tokio::spawn(async move {
-        info!("Signal handler was initialized");
-        while let Some(signal) = signals.next().await {
-            info!("Received signal {signal:?}");
-            if matches!(
-                signal,
-                Ok(Signal::Int) | Ok(Signal::Term) | Ok(Signal::Quit)
-            ) {
-                result_sender.send(()).unwrap();
-                break;
-            }
-        }
-    });
-    Ok(result_receiver)
-}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> std::io::Result<()> {
---
+++ entrypoint/src/signal.rs
@@ -0,0 +1,22 @@
+use crate::info;
+use async_signal::{Signal, Signals};
+use futures_util::StreamExt;
+
+pub fn init_signal_handler() -> std::io::Result<tokio::sync::oneshot::Receiver<()>> {
+    let (result_sender, result_receiver) = tokio::sync::oneshot::channel();
+    let mut signals = Signals::new([Signal::Term, Signal::Int, Signal::Quit])?;
+    tokio::spawn(async move {
+        info!("Signal handler was initialized");
+        while let Some(signal) = signals.next().await {
+            info!("Received signal {signal:?}");
+            if matches!(
+                signal,
+                Ok(Signal::Int) | Ok(Signal::Term) | Ok(Signal::Quit)
+            ) {
+                result_sender.send(()).unwrap();
+                break;
+            }
+        }
+    });
+    Ok(result_receiver)
+}
